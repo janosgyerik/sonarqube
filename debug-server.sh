57f638cf56be59c8eb81d9513397dc4519670010
David Rautureau <david.rautureau@sonarsource.com>
2016-11-16 14:41:09 +0100

    BUILD-282 Rename sonar-application module to build the sonarqube zip Maven artifact

--- debug-server.sh
+++ debug-server.sh
@@ -2,9 +2,9 @@
 
 
 cd sonar-application/target/
-if ! ls sonarqube-*/bin/sonar-application*.jar &> /dev/null; then
+if ! ls sonarqube-*/bin/sonarqube*.jar &> /dev/null; then
   unzip sonarqube-*.zip
 fi
 
 cd sonarqube-*
-java -jar ./lib/sonar-application*.jar -Dsonar.web.javaAdditionalOpts=-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005
+java -jar ./lib/sonarqube*.jar -Dsonar.web.javaAdditionalOpts=-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005
